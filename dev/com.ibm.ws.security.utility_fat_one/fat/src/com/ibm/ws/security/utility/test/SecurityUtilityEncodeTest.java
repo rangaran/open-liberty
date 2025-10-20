2445b2eb9bc371830326cc24e20e3e69245c7d5c
Niyatha Rangarajan <niyatha.rangarajan@ibm.com>
2025-10-20 10:26:41 -0400

    fixing copyright headers

--- dev/com.ibm.ws.security.utility_fat_one/fat/src/com/ibm/ws/security/utility/test/SecurityUtilityEncodeTest.java
+++ dev/com.ibm.ws.security.utility_fat_one/fat/src/com/ibm/ws/security/utility/test/SecurityUtilityEncodeTest.java
@@ -1,14 +1,14 @@
-/*
- * IBM Confidential
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
  *
- * OCO Source Materials
+ * SPDX-License-Identifier: EPL-2.0
  *
- * Copyright IBM Corp. 2016, 2025
- *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
- */
+ *******************************************************************************/
+
 package com.ibm.ws.security.utility.test;
 
 import static org.junit.Assert.assertEquals;
