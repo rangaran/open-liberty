a213770e610a9946610600a98febc4792100a3cd
Jacob de Vos <jacobwdv@ca.ibm.com>
2025-10-30 10:39:48 -0400

    adding debug logging for RC=32 as part of a long build break investigation

--- dev/com.ibm.ws.security.utility_fat_one/fat/src/com/ibm/ws/security/utility/test/SecurityUtilityEncodeTest.java
+++ dev/com.ibm.ws.security.utility_fat_one/fat/src/com/ibm/ws/security/utility/test/SecurityUtilityEncodeTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.After;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.security.SecureRandom;
@@ -22,6 +23,8 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -55,7 +58,7 @@ public class SecurityUtilityEncodeTest {
     private static final String CUSTOM_PASSWORD_ENCRYPTION_EXTENSION_NAME = "customEncryption.jar";
     private static final String PROPERTY_KEY_INSTALL_DIR = "install.dir";
     private static String installDir = null;
-
+    private static final char[] HEX_CHARS = "0123456789ABCDEF".toCharArray();
     // Environment variables
     private static Machine machine;
     private static Properties env;
@@ -142,7 +145,7 @@ public class SecurityUtilityEncodeTest {
 
         Log.info(thisClass, name.getMethodName(), "Executed securityUtility help encode command:" + po.getCommand());
         Log.info(thisClass, name.getMethodName(), "Result:\n" + po.getStdout());
-        
+        printManifestIfRC32(po);
         assertEquals("securityUtility encode help did not result in expected return code.", SUCCESS_RC, po.getReturnCode());
         assertTrue("The expected usage message was not received.", po.getStdout().contains("Usage:"));
         Assert.assertTrue("Help for encode should contain custom encoding feature name.",
@@ -167,6 +170,7 @@ public class SecurityUtilityEncodeTest {
             
         Log.info(thisClass, name.getMethodName(), "Executed securityUtility encode --listCustom command:" + po.getCommand());
         Log.info(thisClass, name.getMethodName(), "Result:\n" + po.getStdout());
+        printManifestIfRC32(po);
         Assert.assertTrue("The output contains the contents of listCustom", po.getStdout().contains(expectedOutput));
     }
 
@@ -191,6 +195,7 @@ public class SecurityUtilityEncodeTest {
 
         Log.info(thisClass, name.getMethodName(), "Executed securityUtility encode command:" + po.getCommand());
         Log.info(thisClass, name.getMethodName(), "Result:\n" + po.getStdout());
+        printManifestIfRC32(po);
         Assert.assertTrue("encode arg result", po.getStdout().contains(encodedText));
 
         // Test custom encoding
@@ -202,6 +207,7 @@ public class SecurityUtilityEncodeTest {
 
         Log.info(thisClass, name.getMethodName(), "Executed securityUtility encode --encoding=custom command:" + po.getCommand());
         Log.info(thisClass, name.getMethodName(), "Result:\n" + po.getStdout());
+        printManifestIfRC32(po);
         Assert.assertTrue("encode arg result", po.getStdout().contains(customEncodedText));
     }
 
@@ -273,7 +279,7 @@ public class SecurityUtilityEncodeTest {
         Log.info(thisClass, name.getMethodName(), "Output for encode with --base64Key: " + po.getStdout()); 
         Log.info(thisClass, name.getMethodName(), "Executed encode with --base64Key: " + po.getCommand());
         Log.info(thisClass, name.getMethodName(), "Result:\n" + po.getStdout() + "\nError:\n" + po.getStderr());
-
+        printManifestIfRC32(po);
         assertEquals("securityUtility encode with --base64Key should succeed", SUCCESS_RC, po.getReturnCode());
         assertTrue("Output should contain AES encoded value", po.getStdout().contains("{aes}"));
 
@@ -298,7 +304,7 @@ public class SecurityUtilityEncodeTest {
                 "--aesConfigFile=" + validKeyFile.getAbsolutePath()
             },
             betaEnv);
-
+        printManifestIfRC32(po2);
         assertEquals("securityUtility encode unexpectedly failed with valid AES key.", SUCCESS_RC, po2.getReturnCode());
 
     }
@@ -497,7 +503,99 @@ public void testMutuallyExclusiveKeyOptions_base64KeyAndSecureKeyFile() throws E
                output.contains("CWWKS1861E"));
 }
 
+    /**
+     * Prints a byte array as hexadecimal values to System.out
+     *
+     * @param bytes The byte array to print
+     */
+    public static String printHexValues(byte[] bytes) {
+        if (bytes == null) {
+            Log.info(thisClass, name.getMethodName(), "Error: Null byte array provided");
+            return null;
+        }
 
+        if (bytes.length == 0) {
+            Log.info(thisClass, name.getMethodName(), "Empty byte array");
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[i];
+            // Convert each byte to two hex characters
+            char firstChar = HEX_CHARS[(b & 0xF0) >>> 4];
+            char secondChar = HEX_CHARS[b & 0x0F];
+
+            sb.append(firstChar);
+            sb.append(secondChar);
+
+            // Add space between bytes for readability
+            if (i < bytes.length - 1) {
+                sb.append(' ');
+            }
+
+            // Add a line break every 16 bytes for better readability
+            if ((i + 1) % 16 == 0 && i < bytes.length - 1) {
+                sb.append(System.lineSeparator());
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Converts a byte array to a hexadecimal string
+     *
+     * @param bytes The byte array to convert
+     * @return A string containing the hexadecimal representation of the byte array, or null if the input is null
+     */
+    public static String bytesToHexString(byte[] bytes) {
+        if (bytes == null) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder(bytes.length * 2);
+        for (byte b : bytes) {
+            sb.append(HEX_CHARS[(b & 0xF0) >>> 4]);
+            sb.append(HEX_CHARS[b & 0x0F]);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * @param po
+     * @throws IOException
+     */
+    private void printManifestIfRC32(ProgramOutput po) throws Exception {
+        if (po.getReturnCode() == 32) {
+            RemoteFile customJar = server.getFileFromLibertyInstallRoot(CUSTOM_PASSWORD_ENCRYPTION_EXTENSION_PATH + File.separator + CUSTOM_PASSWORD_ENCRYPTION_EXTENSION_NAME);
+
+            try (ZipInputStream zis = new ZipInputStream(customJar.openForReading())) {
+                ZipEntry entry;
+                while ((entry = zis.getNextEntry()) != null) {
+                    if (!entry.isDirectory()) {
+                        Log.info(thisClass, name.getMethodName(), "Processing file: " + entry.getName());
+
+                        // Create an in-memory output stream to capture the data
+                        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+
+                            byte[] buffer = new byte[4096];
+                            int len;
+                            while ((len = zis.read(buffer)) > 0) {
+                                baos.write(buffer, 0, len);
+                            }
+                            byte[] entryContentBytes = baos.toByteArray();
+                            Log.info(thisClass, name.getMethodName(), new String(entryContentBytes));
+                            Log.info(thisClass, name.getMethodName(), "\nHex:\n" + printHexValues(entryContentBytes));
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 }
 
 
