607ed0a2d9568dfee9bb9d047a1251eb8590fc9c
Phu Dinh <pmdinh@us.ibm.com>
2025-12-08 14:14:10 -0500

    Fix test bug in WebContainer Servlet 31 FAT CDIUpgradeHandlerTest

--- dev/com.ibm.ws.webcontainer.servlet.3.1_fat/test-applications/CDI12TestV2Upgrade.war/src/com/ibm/ws/webcontainer/servlet_31_fat/cdi12testv2upgrade/war/cdi/upgrade/handlers/CDITestHttpUpgradeHandler.java
+++ dev/com.ibm.ws.webcontainer.servlet.3.1_fat/test-applications/CDI12TestV2Upgrade.war/src/com/ibm/ws/webcontainer/servlet_31_fat/cdi12testv2upgrade/war/cdi/upgrade/handlers/CDITestHttpUpgradeHandler.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -263,6 +263,10 @@ public class CDITestHttpUpgradeHandler implements HttpUpgradeHandler {
         logState(methodName);
 
         logBeanActivity(methodName, "Exit");
+
+        logInfo(methodName, "null out readListener [" + readListener + "] , writeListener [" + writeListener + "]");
+        readListener = null;
+        writeListener = null;
 
         logExit(methodName);
     }
