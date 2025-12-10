5bf61be50c0f4154c3987bf293d6d87701be481e
Benjamin Confino <benjamic@uk.ibm.com>
2025-12-10 09:23:22 +0000

    Create SibIntrospectorImpl

---
+++ dev/com.ibm.ws.messaging.runtime/src/com/ibm/ws/sib/admin/internal/SibIntrospectorImpl.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package com.ibm.ws.sib.admin.internal;
+
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Enumeration;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Reference;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.sib.admin.JsMain;
+import com.ibm.ws.sib.admin.JsMessagingEngine;
+import com.ibm.ws.sib.utils.ras.FormattedWriter;
+import com.ibm.wsspi.logging.Introspector;
+
+@Component( immediate = true,
+configurationPolicy = ConfigurationPolicy.IGNORE,
+property = { Constants.SERVICE_VENDOR + "=" + "IBM" } )
+public class SibIntrospectorImpl implements Introspector {
+    private static final TraceComponent tc = Tr.register(SibIntrospectorImpl.class);
+    
+    /**
+     * This spec controls what will be dumped. It splits on ":" into tokens and then dumps classNames that match the token. Only these two classes will be searched for
+     * MessageStoreImpl can be upgraded to MessageStoreImpl=all but I think the null value is what we want. 
+     */
+    private final static String DUMP_SPEC = "com.ibm.ws.sib.processor.impl.MessageProcessor:com.ibm.ws.sib.msgstore.impl.MessageStoreImpl";
+
+    @Reference
+    JsMain jsMain;
+
+    @Override
+    public String getIntrospectorName() {
+        return "MessagingIntrospector";
+    }
+
+    @Override
+    public String getIntrospectorDescription(){
+        return "Messaging Diagnostics";
+    }
+
+    @Override
+    public void introspect(PrintWriter out) throws Exception{
+    	
+    	if (! (jsMain instanceof JsMainImpl)) {
+    		out.println();
+            out.println("The JsMain class was not an instance of JsMainImpl");
+            return;
+    	}
+    	
+    	JsMainImpl mainImpl = (JsMainImpl) jsMain;
+    	Enumeration<JsMessagingEngine> engines = mainImpl.listMessagingEngines();
+    	
+    	while (engines.hasMoreElements()) {
+    		JsMessagingEngine engine = engines.nextElement();
+    		
+    		if (engine instanceof JsMessagingEngineImpl) {
+    			JsMessagingEngineImpl jsEngine = (JsMessagingEngineImpl) engine;
+    			
+    			jsEngine.dump(DUMP_SPEC, new FormattedWriter(out), new Date());
+    		}
+    	}
+    }
+}
