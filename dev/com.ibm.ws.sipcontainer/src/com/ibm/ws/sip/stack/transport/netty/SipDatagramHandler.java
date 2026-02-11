7262d0850b0622dd1d65f445a0490ad42b000499
Isaac A. Rivera Rivas <isaacrivriv@gmail.com>
2026-02-11 13:01:29 -0500

    Added logic for handling SIP message processing in Liberty threads to avoid deadlock conditions

--- dev/com.ibm.ws.sipcontainer/src/com/ibm/ws/sip/stack/transport/netty/SipDatagramHandler.java
+++ dev/com.ibm.ws.sipcontainer/src/com/ibm/ws/sip/stack/transport/netty/SipDatagramHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
+ * Copyright (c) 2021, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -9,14 +9,20 @@
  *******************************************************************************/
 package com.ibm.ws.sip.stack.transport.netty;
 
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.sip.stack.transport.GenericEndpointImpl;
 import com.ibm.ws.sip.stack.transport.sip.netty.*;
 
+import io.netty.channel.ChannelConfig;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
+import io.netty.util.ReferenceCountUtil;
 import jain.protocol.ip.sip.ListeningPoint;
 
 public class SipDatagramHandler extends SimpleChannelInboundHandler<SipMessageEvent> {
@@ -24,6 +30,14 @@ public class SipDatagramHandler extends SimpleChannelInboundHandler<SipMessageEv
     private static final TraceComponent tc = Tr.register(SipDatagramHandler.class);
 
     final private AttributeKey<SipUdpConnLink> attrKey = AttributeKey.valueOf("SipUdpConnLink");
+
+    private boolean processingMessage = false;
+    private static final int DEFAULT_MAX_QUEUE = 50;
+    private final LinkedBlockingQueue<SipMessageEvent> messageQueue;
+
+    public SipDatagramHandler() {
+        this.messageQueue = new LinkedBlockingQueue<SipMessageEvent>(DEFAULT_MAX_QUEUE);
+    }
 
     /**
      * Called when a new connection is established
@@ -55,24 +69,6 @@ public class SipDatagramHandler extends SimpleChannelInboundHandler<SipMessageEv
     }
 
     @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-            Tr.debug(this, tc, "channelInactive", ctx.channel().remoteAddress() + " has been disconnected");
-        }
-
-        Attribute<SipUdpConnLink> attr = ctx.channel().attr(attrKey);
-        SipUdpConnLink connLink = attr.get();
-        // clean up from connections table
-        if (connLink != null) {
-            connLink.close();
-        } else {
-            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-                Tr.debug(this, tc, "channelInactive", ctx.name() + "could not find a SIP channel");
-            }
-        }
-    }
-
-    @Override
     protected void channelRead0(ChannelHandlerContext ctx, SipMessageEvent msg) throws Exception {
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
             Tr.debug(this, tc, "channelRead0. " + ctx.channel() + ". [" + msg.getSipMsg().getMarkedBytesNumber()
@@ -80,11 +76,62 @@ public class SipDatagramHandler extends SimpleChannelInboundHandler<SipMessageEv
         }
         Attribute<SipUdpConnLink> attr = ctx.channel().attr(attrKey);
         SipUdpConnLink connLink = attr.get();
+        if (connLink == null) {
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                Tr.debug(this, tc, "processNextMessage", "could not associate an incoming message with a SIP channel");
+            }
+            throw new IOException("could not associate an incoming message with a SIP channel");
+        }
+        messageQueue.offer(ReferenceCountUtil.retain(msg));
+        if (processingMessage) {
+            if (messageQueue.remainingCapacity() == 0) {
+                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                    Tr.debug(this, tc, "Queue reached capacity. Reads are paused.");
+                }
+                pauseReading(ctx);
+            }
+        } else {
+            processingMessage = true;
+            processNextMessage(ctx, connLink);
+        }
+    }
+
+    // This is designed so that the logic besides what runs in the Liberty executor pool
+    // needs to run on the event loop, otherwise there could be race conditions that show up.
+    private void processNextMessage(final ChannelHandlerContext ctx, final SipUdpConnLink connLink) {
+        SipMessageEvent msg = messageQueue.poll();
+        // Continue reading again if isn't autoread and we have space to queue more messages
+        if(!ctx.channel().config().isAutoRead() && messageQueue.remainingCapacity() > DEFAULT_MAX_QUEUE/2){
+            resumeReading(ctx);
+        }
+        if (msg == null) {
+            processingMessage = false;
+            return;
+        }
+        GenericEndpointImpl.getExecutorService().execute(() -> {
+            try {
+                connLink.complete(msg.getSipMsg(), msg.getRemoteAddress());
+            } finally {
+                ReferenceCountUtil.release(msg);
+            }
+            ctx.channel().eventLoop().execute(() -> processNextMessage(ctx, connLink));
+        });
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(this, tc, "channelInactive", ctx.channel().remoteAddress() + " has been disconnected");
+        }
+        emptyMessageQueue();
+        Attribute<SipUdpConnLink> attr = ctx.channel().attr(attrKey);
+        SipUdpConnLink connLink = attr.get();
+        // clean up from connections table
         if (connLink != null) {
-            connLink.complete(msg.getSipMsg(), msg.getRemoteAddress());
+            GenericEndpointImpl.getExecutorService().execute(() -> connLink.close());
         } else {
             if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-                Tr.debug(this, tc, "channelRead0", "could not find a SIP channel");
+                Tr.debug(this, tc, "channelInactive", ctx.name() + "could not find a SIP channel");
             }
         }
     }
@@ -94,11 +141,12 @@ public class SipDatagramHandler extends SimpleChannelInboundHandler<SipMessageEv
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
             Tr.debug(this, tc, "exceptionCaught. " + cause.getClass());
         }
+        emptyMessageQueue();
         Attribute<SipUdpConnLink> attr = ctx.channel().attr(attrKey);
         if (cause instanceof Exception) {
             SipUdpConnLink connLink = attr.get();
             if (connLink != null) {
-                connLink.close(cause);
+                GenericEndpointImpl.getExecutorService().execute(() -> connLink.close(cause));
             } else {
                 if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                     Tr.debug(this, tc, "exceptionCaught", "could not find a SIP channel");
@@ -107,5 +155,27 @@ public class SipDatagramHandler extends SimpleChannelInboundHandler<SipMessageEv
         }
         ctx.close();
     }
+
+    private void emptyMessageQueue() {
+        SipMessageEvent msg;
+        while ((msg = messageQueue.poll()) != null) {
+            ReferenceCountUtil.safeRelease(msg);
+        }
+    }
+
+    private void pauseReading(ChannelHandlerContext context) {
+        ChannelConfig config = context.channel().config();
+        if (config.isAutoRead()) {
+            config.setAutoRead(false);
+        }
+    }
+
+    private void resumeReading(ChannelHandlerContext context) {
+        ChannelConfig config = context.channel().config();
+        if (!config.isAutoRead()) {
+            config.setAutoRead(true);
+        }
+    }
+
 }
 
--- dev/com.ibm.ws.sipcontainer/src/com/ibm/ws/sip/stack/transport/netty/SipStreamHandler.java
+++ dev/com.ibm.ws.sipcontainer/src/com/ibm/ws/sip/stack/transport/netty/SipStreamHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
+ * Copyright (c) 2021, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -9,15 +9,20 @@
  *******************************************************************************/
 package com.ibm.ws.sip.stack.transport.netty;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.sip.stack.transaction.transport.connections.SipMessageByteBuffer;
+import com.ibm.ws.sip.stack.transport.GenericEndpointImpl;
 import com.ibm.ws.sip.stack.transport.sip.netty.*;
 
+import io.netty.channel.ChannelConfig;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
+import io.netty.util.ReferenceCountUtil;
 import jain.protocol.ip.sip.ListeningPoint;
 import java.io.IOException;
 
@@ -26,6 +31,14 @@ public class SipStreamHandler extends SimpleChannelInboundHandler<SipMessageByte
     private static final TraceComponent tc = Tr.register(SipStreamHandler.class);
 
     final private AttributeKey<SipTcpInboundConnLink> attrKey = AttributeKey.valueOf("SipTcpInboundConnLink");
+
+    private boolean processingMessage = false;
+    private static final int DEFAULT_MAX_QUEUE = 50;
+    private final LinkedBlockingQueue<SipMessageByteBuffer> messageQueue;
+
+    public SipStreamHandler() {
+        this.messageQueue = new LinkedBlockingQueue<SipMessageByteBuffer>(DEFAULT_MAX_QUEUE);
+    }
 
     /**
      * Called when a new connection is established
@@ -62,16 +75,46 @@ public class SipStreamHandler extends SimpleChannelInboundHandler<SipMessageByte
         }
         Attribute<SipTcpInboundConnLink> attr = ctx.channel().attr(attrKey);
         SipTcpInboundConnLink connLink = attr.get();
-
-        if (connLink != null) {
-            connLink.complete(msg);
-        } else {
+        if (connLink == null) {
             if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-                Tr.debug(this, tc, "channelRead0", "could not associate an incoming message with a SIP channel");
+                Tr.debug(this, tc, "processNextMessage", "could not associate an incoming message with a SIP channel");
             }
             throw new IOException("could not associate an incoming message with a SIP channel");
         }
+        messageQueue.offer(ReferenceCountUtil.retain(msg));
+        if (processingMessage) {
+            if (messageQueue.remainingCapacity() == 0) {
+                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                    Tr.debug(this, tc, "Queue reached capacity. Reads are paused.");
+                }
+                pauseReading(ctx);
+            }
+        } else {
+            processingMessage = true;
+            processNextMessage(ctx, connLink);
+        }
+    }
 
+    // This is designed so that the logic besides what runs in the Liberty executor pool
+    // needs to run on the event loop, otherwise there could be race conditions that show up.
+    private void processNextMessage(final ChannelHandlerContext ctx, final SipTcpInboundConnLink connLink){
+        SipMessageByteBuffer msg = messageQueue.poll();
+        // Continue reading again if isn't autoread and we have space to queue more messages
+        if(!ctx.channel().config().isAutoRead() && messageQueue.remainingCapacity() > DEFAULT_MAX_QUEUE/2){
+            resumeReading(ctx);
+        }
+        if (msg == null) {
+            processingMessage = false;
+            return;
+        }
+        GenericEndpointImpl.getExecutorService().execute(() -> {
+            try {
+                connLink.complete(msg);
+            } finally {
+                ReferenceCountUtil.release(msg);
+            }
+            ctx.channel().eventLoop().execute(() -> processNextMessage(ctx, connLink));
+        });
     }
 
     @Override
@@ -79,12 +122,12 @@ public class SipStreamHandler extends SimpleChannelInboundHandler<SipMessageByte
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
             Tr.debug(this, tc, "channelInactive", ctx.channel().remoteAddress() + " has been disconnected");
         }
-
+        emptyMessageQueue();
         Attribute<SipTcpInboundConnLink> attr = ctx.channel().attr(attrKey);
         SipTcpInboundConnLink connLink = attr.get();
         // clean up from connections table
         if (connLink != null) {
-            connLink.destroy();
+            GenericEndpointImpl.getExecutorService().execute(() -> connLink.destroy());
         } else {
             if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                 Tr.debug(this, tc, "channelInactive", "could not find a SIP channel");
@@ -97,11 +140,12 @@ public class SipStreamHandler extends SimpleChannelInboundHandler<SipMessageByte
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
             Tr.debug(this, tc, "exceptionCaught. " + cause.getClass());
         }
+        emptyMessageQueue();
         Attribute<SipTcpInboundConnLink> attr = ctx.channel().attr(attrKey);
         if (cause instanceof Exception) {
             SipTcpInboundConnLink connLink = attr.get();
             if (connLink != null) {
-                connLink.destroy((Exception) cause);
+                GenericEndpointImpl.getExecutorService().execute(() -> connLink.destroy((Exception) cause));
             } else {
                 if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                     Tr.debug(this, tc, "exceptionCaught", "could not find a SIP channel");
@@ -110,4 +154,26 @@ public class SipStreamHandler extends SimpleChannelInboundHandler<SipMessageByte
         }
         ctx.close();
     }
+
+    private void emptyMessageQueue() {
+        SipMessageByteBuffer msg;
+        while ((msg = messageQueue.poll()) != null) {
+            ReferenceCountUtil.safeRelease(msg);
+        }
+    }
+
+    private void pauseReading(ChannelHandlerContext context) {
+        ChannelConfig config = context.channel().config();
+        if (config.isAutoRead()) {
+            config.setAutoRead(false);
+        }
+    }
+
+    private void resumeReading(ChannelHandlerContext context) {
+        ChannelConfig config = context.channel().config();
+        if (!config.isAutoRead()) {
+            config.setAutoRead(true);
+        }
+    }
+
 }
