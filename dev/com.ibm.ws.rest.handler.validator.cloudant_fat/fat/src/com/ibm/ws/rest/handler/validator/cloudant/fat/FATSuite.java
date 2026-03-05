016a548bacd6b85fef9d09805d001d0aea40e125
Paul Nicolucci <pnicolucci@gmail.com>
2026-03-05 14:15:22 -0500

    Revert "Repeat for Jakarta EE11- Phase 2 - Part 1"

--- dev/com.ibm.ws.rest.handler.validator.cloudant_fat/fat/src/com/ibm/ws/rest/handler/validator/cloudant/fat/FATSuite.java
+++ dev/com.ibm.ws.rest.handler.validator.cloudant_fat/fat/src/com/ibm/ws/rest/handler/validator/cloudant/fat/FATSuite.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2026 IBM Corporation and others.
+ * Copyright (c) 2019, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package com.ibm.ws.rest.handler.validator.cloudant.fat;
 
@@ -16,7 +19,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import componenttest.containers.TestContainerSuite;
 import componenttest.custom.junit.runner.AlwaysPassesTest;
-import componenttest.rules.repeater.FeatureReplacementAction;
+import componenttest.rules.repeater.JakartaEE10Action;
+import componenttest.rules.repeater.JakartaEE9Action;
 import componenttest.rules.repeater.RepeatTests;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.utils.HttpsRequest;
@@ -29,9 +33,8 @@ public class FATSuite extends TestContainerSuite {
 
     @ClassRule
     public static RepeatTests r = RepeatTests.withoutModification() // run all tests as-is (e.g. EE8 features)
-                    .andWith(FeatureReplacementAction.EE9_FEATURES().alwaysAddFeature("servlet-5.0")) // run all tests again with EE9 features+packages
-                    .andWith(FeatureReplacementAction.EE10_FEATURES().alwaysAddFeature("servlet-6.0"))
-                    .andWith(FeatureReplacementAction.EE11_FEATURES().alwaysAddFeature("servlet-6.1"));
+                    .andWith(new JakartaEE9Action().alwaysAddFeature("servlet-5.0")) // run all tests again with EE9 features+packages
+                    .andWith(new JakartaEE10Action().alwaysAddFeature("servlet-6.0"));
 
     static {
         // TODO: temporary debug setting so we can further investigate intermittent
