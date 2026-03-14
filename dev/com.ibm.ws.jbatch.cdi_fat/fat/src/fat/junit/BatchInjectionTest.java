c13eeb3dadb944dcb880c7894ac8b78c95f6dc7f
Paul Nicolucci <pnicolucci@gmail.com>
2026-03-14 17:32:28 -0400

    Repeat for Jakarta EE11

--- dev/com.ibm.ws.jbatch.cdi_fat/fat/src/fat/junit/BatchInjectionTest.java
+++ dev/com.ibm.ws.jbatch.cdi_fat/fat/src/fat/junit/BatchInjectionTest.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2023 IBM Corporation and others.
+ * Copyright (c) 2017, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package fat.junit;
 
@@ -62,8 +59,9 @@ public class BatchInjectionTest extends FATServletClient {
     @ClassRule
     public static RepeatTests r = RepeatTests.withoutModification()
                     .andWith(FeatureReplacementAction.EE8_FEATURES().forServers("BatchInjection"))
-                    .andWith(new JakartaEE9Action().forServers("BatchInjection"))
-                    .andWith(new JakartaEE10Action().forServers("BatchInjection"));
+                    .andWith(FeatureReplacementAction.EE9_FEATURES().forServers("BatchInjection"))
+                    .andWith(FeatureReplacementAction.EE10_FEATURES().forServers("BatchInjection"))
+                    .andWith(FeatureReplacementAction.EE11_FEATURES().forServers("BatchInjection));
 
     @Server("BatchInjection")
     @TestServlet(servlet = BatchInjectionServlet.class, path = "implicit/BatchInjectionServlet")
