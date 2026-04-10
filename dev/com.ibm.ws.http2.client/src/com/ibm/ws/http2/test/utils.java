528ffc68054a6e9391333d4ce81f59e20ee97012
Isaac A. Rivera Rivas <isaacrivriv@gmail.com>
2026-04-10 12:14:26 -0400

    Added changes to increase the default frame read timeout on the H2 client

--- dev/com.ibm.ws.http2.client/src/com/ibm/ws/http2/test/utils.java
+++ dev/com.ibm.ws.http2.client/src/com/ibm/ws/http2/test/utils.java
@@ -18,7 +18,7 @@ import com.ibm.wsspi.bytebuffer.WsByteBuffer;
 
 public class utils {
 
-    public static final int IO_DEFAULT_TIMEOUT = 28000;
+    public static final int IO_DEFAULT_TIMEOUT = 40000;
     public static final int IO_DEFAULT_BUFFER_SIZE = 4090;
 
     public static final byte FRAME_TYPE_DATA = 0x00;
