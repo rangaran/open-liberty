770c4410b770d589e435e2a5592c311359b8de5c
Emanuel Martinez Vazquez <emanuel.martinez@ibm.com>
2026-04-16 10:23:36 -0400

    Defining the new properties in the metatype and changing copy rights

--- dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/osgi/mbeans/PluginGenerator.java
+++ dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/osgi/mbeans/PluginGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2023 IBM Corporation and others.
+ * Copyright (c) 2009, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -335,10 +335,12 @@ public class PluginGenerator {
             rootElement.appendChild(esiProp5);
 
             // Add OutboundInterfacesList property if configured
-            Element outboundInterfacesProp = output.createElement("Property");
-            outboundInterfacesProp.setAttribute("Name", "OutboundInterfacesList");
-            outboundInterfacesProp.setAttribute("Value", pcd.OutboundInterfacesList);
-            rootElement.appendChild(outboundInterfacesProp);
+            if (pcd.OutboundInterfacesList != null) {
+                Element outboundInterfacesProp = output.createElement("Property");
+                outboundInterfacesProp.setAttribute("Name", "OutboundInterfacesList");
+                outboundInterfacesProp.setAttribute("Value", pcd.OutboundInterfacesList);
+                rootElement.appendChild(outboundInterfacesProp);
+            }
 
             // Add OutboundBindStrict property
             Element outboundBindStrictProp = output.createElement("Property");
