3a0a6e815b4422180dd5406748136a9133a7927b
yasmin-aumeeruddy <yasmin.aumeeruddy@ibm.com>
2026-04-21 22:03:42 +0100

    Add comment for JVM metrics check in otelCollectorQueryClient.java

--- dev/io.openliberty.microprofile.telemetry.1.0.internal.container_fat/fat/src/io/openliberty/microprofile/telemetry/internal/utils/otelCollector/OtelCollectorQueryClient.java
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal.container_fat/fat/src/io/openliberty/microprofile/telemetry/internal/utils/otelCollector/OtelCollectorQueryClient.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 IBM Corporation and others.
+ * Copyright (c) 2024, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -61,7 +61,7 @@ public class OtelCollectorQueryClient {
         metricsFound.put("JvmMemoryUsedAfterLastGc", false);
         metricsFound.put("JvmMemoryLimit", false);
         metricsFound.put("JvmThreadCount", false);
-        //Finds each expected JVM metric in the OpenTelemetry Collector output
+        //Finds each expected JVM metric in the OpenTelemetry Collector output. Names have slightly changed since OpenTelemetry Collector 0.40.0. E.g. jvm_class_unloaded ->  jvm_class_unloaded_total
         for (String s : splits) {
             if (s.contains("jvm_class_count Number of classes currently loaded.")) {
                 metricsFound.put("JvmClassCount", true);
