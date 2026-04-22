adf70bc15c62e62a6c50f50693e19c735bdba94b
Adam Anderson <atanderson9383@gmail.com>
2026-04-22 09:58:57 -0500

    [34687] - Add unchanged EE10 RESTEasy overlay files from upstream
    Added vanilla overlay files from RESTEasy 6.2.8.Final:
    - MethodInjectorImpl.java - Handles method invocation
    - CdiConstructorInjector.java - CDI constructor injection
    - CdiInjectorFactory.java - CDI injector factory
    - CdiPropertyInjector.java - CDI property injection
    - Utils.java - Helper methods
    
    Files copied unchanged from upstream to establish baseline for Liberty changes.
    
    Co-authored-by-AI: IBM Bob 1.0.1

---
+++ dev/io.openliberty.org.jboss.resteasy.cdi.ee10/src/org/jboss/resteasy/cdi/CdiConstructorInjector.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright The RESTEasy Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Source: https://github.com/resteasy/resteasy/blob/6.2.8.Final/resteasy-cdi/src/main/java/org/jboss/resteasy/cdi/CdiConstructorInjector.java
+
+package org.jboss.resteasy.cdi;
+
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.enterprise.context.spi.CreationalContext;
+import jakarta.enterprise.inject.spi.Bean;
+import jakarta.enterprise.inject.spi.BeanManager;
+import jakarta.ws.rs.WebApplicationException;
+
+import org.jboss.resteasy.cdi.i18n.LogMessages;
+import org.jboss.resteasy.cdi.i18n.Messages;
+import org.jboss.resteasy.spi.ApplicationException;
+import org.jboss.resteasy.spi.ConstructorInjector;
+import org.jboss.resteasy.spi.Failure;
+import org.jboss.resteasy.spi.HttpRequest;
+import org.jboss.resteasy.spi.HttpResponse;
+
+/**
+ * This ConstructorInjector implementation uses CDI's BeanManager to obtain
+ * a contextual instance of a bean.
+ *
+ * @author Jozef Hartinger
+ *
+ */
+public class CdiConstructorInjector implements ConstructorInjector {
+    private final BeanManager manager;
+    private final Type type;
+
+    public CdiConstructorInjector(final Type type, final BeanManager manager) {
+        this.type = type;
+        this.manager = manager;
+    }
+
+    @Override
+    public Object construct(boolean unwrapAsync) {
+        Set<Bean<?>> beans = manager.getBeans(type);
+
+        if (beans.size() > 1) {
+            Set<Bean<?>> modifiableBeans = new HashSet<>(beans);
+            // Ambiguous dependency may occur if a resource has subclasses
+            // Therefore we remove those beans
+            // remove Beans that have clazz in their type closure but not as a base class
+            modifiableBeans.removeIf(bean -> !bean.getBeanClass().equals(type) && !bean.isAlternative());
+            beans = modifiableBeans;
+        }
+
+        if (LogMessages.LOGGER.isDebugEnabled()) //keep this check for performance reasons, as toString() is expensive on CDI Bean
+        {
+            LogMessages.LOGGER.debug(Messages.MESSAGES.beansFound(type, beans));
+        }
+
+        Bean<?> bean = manager.resolve(beans);
+        CreationalContext<?> context = manager.createCreationalContext(bean);
+        return manager.getReference(bean, type, context);
+    }
+
+    @Override
+    public Object construct(HttpRequest request, HttpResponse response, boolean unwrapAsync)
+            throws Failure, WebApplicationException, ApplicationException {
+        return construct(unwrapAsync);
+    }
+
+    @Override
+    public Object injectableArguments(boolean unwrapAsync) {
+        return null;
+    }
+
+    @Override
+    public Object injectableArguments(HttpRequest request, HttpResponse response, boolean unwrapAsync) throws Failure {
+        return injectableArguments(unwrapAsync);
+    }
+}
